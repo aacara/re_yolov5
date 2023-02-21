2208af397fa1e7d447a119f0b83e32572e26ff71
aacara <budgielover@naver.com>
2023-02-21 19:45:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -32,6 +32,8 @@ def imageInput(src):
                 model = torch.hub.load('ultralytics/yolov5', 'custom', path='runs/cons0205/weights/best.pt', force_reload=True)
                 # model.cuda() if device == 'cuda' else model.cpu()
                 #-pred = model(imgpath)
+                with open(image_file, mode="wb") as f:
+                    f.write(image_file.getbuffer())
                 pred = model(image_file)
                 pred.render()  # render bbox in image
                 for im in pred.ims:
